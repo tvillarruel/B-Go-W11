276592873ea7ba6cb5ed2dce62630453dd84ce1a
tvillarruel <thiago.villarruel@mercadolibre.com>
2023-12-28 09:55:36 -0300

    feat: added fmt and variables tests

--- main/main.go
+++ main/main.go
@@ -4,6 +4,24 @@ import "fmt"
 
 func main() {
 
-	fmt.Print("Hello World")
+	// fmt.Print("Hello World")
+
+	//-------------------------
 	
+	/* 
+	var name string
+	var age int = 22
+	const country string = "Argentina"
+
+	fmt.Print("Hello, tell me your name please: ")
+	fmt.Scan(&name)
+	fmt.Println(name, " I know your age is: ", age, " and you live in: ", country) 
+	*/
+	 
+	//-------------------------
+
+	
+	 
+
+
 }
