6905b643415ef4b914a5c9d675dc781f2de69d18
tvillarruel <thiago.villarruel@mercadolibre.com>
2023-12-28 15:03:21 -0300

    feat: added simple loop tests

--- main/main.go
+++ main/main.go
@@ -4,12 +4,11 @@ import "fmt"
 
 func main() {
 
-	// fmt.Print("Hello World")
-
 	//-------------------------
 
 	/* 
-	VAR, CONST AND FMT TESTS
+
+	// VAR, CONST AND FMT TESTS
 
 	var name string
 	var age int = 22
@@ -21,6 +20,10 @@ func main() {
 	*/
 	 
 	//-------------------------
+
+	/*
+
+	// MULTIPLE VAR AND CONSTS
 
 	var (
 		firstName = "Thiago"
@@ -36,10 +39,56 @@ func main() {
 	)
 
 	fmt.Print("Hello, my full name is ", firstName, lastName," and I am ", age," years old. \n I measure ", height, ". \n Am I active?: ", active, "\n I live in ", country, " and today we are in ", currentYear)
+	*/
 
+	//-------------------------
 
+	/* 
 
+	// IF .. ELSE
 
+	var (
+		a = -1
+		b = -1
+		c = -1
+		d = 1
+	)
+
+	if a < b && c < d {
+		fmt.Println("Primer output")
+	} else if a > b || c > d {
+		fmt.Println("Segundo output")
+	} else if d == 0 {
+		fmt.Println("Tercer output")
+	} else {
+		fmt.Println("Cuarto output")
+	}
+	*/
+	
+	/*
+	
+	// SWITCH
+
+	var op int
+
+	fmt.Println("Ingresa un numero del 1 al 4")
+	fmt.Scan(&op)
+	
+	switch op {
+		case 1:
+			fmt.Println("Uno")
+		case 2:
+			fmt.Println("Dos")
+		case 3:
+			fmt.Println("Tres")
+		case 4:
+			fmt.Println("Cuatro")
+		default:
+			fmt.Println("Cualquier otro numero")
+	}
+	*/
+
+	fmt.Print("Hello World")
 
 	
 	 
