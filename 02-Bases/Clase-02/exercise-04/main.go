22b3bb9950a26db8ad96413d539a221141ddc3a0
tvillarruel <thiago.villarruel@mercadolibre.com>
2024-01-02 10:54:21 -0300

    fix: removed comments on exercise 04

--- 02-Bases/Clase-02/exercise-04/main.go
+++ 02-Bases/Clase-02/exercise-04/main.go
@@ -3,17 +3,8 @@ package main
 import "fmt"
 
 func main(){
-	/*
-	Un empleado de una empresa quiere saber el nombre y edad de uno de sus empleados. Según el siguiente mapa, ayudá a imprimir la edad de Benjamin.
-	var employees = map[string]int{"Benjamin": 20, "Nahuel": 26, "Brenda": 19, "Darío": 44, "Pedro": 30}
-	
-	Por otro lado también es necesario:
-	
-	Saber cuántos de sus empleados son mayores de 21 años.
-	Agregar un empleado nuevo a la lista, llamado Federico que tiene 25 años.
-	Eliminar a Pedro del mapa.
-	*/
 
+	
 	var employees = map[string]int{"Benjamin": 20, "Nahuel": 26, "Brenda": 19, "Darío": 44, "Pedro": 30}
 
 	fmt.Println("Benjamin's age: ", employees["Benjamin"])
